a84a1a15e8622785b8d1438dbac62b3484c0617f
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-11-26 16:17:52 +0530

    added redux

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -2,16 +2,13 @@ import { callAPI } from "../config/axiosInstance.js";
 
 const userEndpoints = {
     Profile: "/users/profile",
-    UserList: "/users/login",
     UserUpdate: "/users/profile/update",
-    FindUserBuId:"/users/send-mail-to-verify",
-    UserDelete:"/users/verify-mail",
 };
 
 
 const userApi = {
     profile: async (params) => await callAPI("get", userEndpoints.Profile,params),
-    update: async (params) => await callAPI("post", userEndpoints.UserUpdate, params),    //UserList: async (params) => await callAPI("post", userEndpoints.SignIn, params),
+    update: async (params) => await callAPI("post", userEndpoints.UserUpdate, params),
 };
 
 export default userApi;
